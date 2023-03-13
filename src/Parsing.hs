65d958a10361d6db6ecd827604422be40b757ea1
Alex <alexandrualin1010@gmail.com>
2023-03-13 15:39:28 +0200

    stack 2

--- src/Parsing.hs
+++ src/Parsing.hs
@@ -37,7 +37,7 @@ lambdaExp :: Parser ComplexExp
 lambdaExp = do
     reservedOp miniHs "\\"
     v <- var
-    reservedOp miniHs "->"
+    (reservedOp miniHs) "->"
     cex <- expr
     return $ CLam v cex
 
@@ -46,11 +46,11 @@ lambdaExp = do
 
 letExp :: Parser ComplexExp
 letExp = do
-    reserved miniHs "let"
+    (reserved miniHs) "let"
     v <- var
-    reservedOp miniHs "="
+    (reservedOp miniHs) "="
     c1 <- expr
-    reserved miniHs "in"
+    (reserved miniHs) "in"
     c2 <- expr
     return $ Let v c1 c2
 -- >>> testParse letExp "let x := y in z"
@@ -79,7 +79,7 @@ natExp = Nat <$> fromInteger <$> (natural miniHs)
 -- Nat 223
 
 parenExp :: Parser ComplexExp
-parenExp = undefined
+parenExp = (parens miniHs) expr
 -- >>> ghci> testParse parenExp "(a)"
 -- CX (Var {getVar = "a"})
 
@@ -88,8 +88,15 @@ basicExp = letrecExp <|> letExp <|> lambdaExp <|> varExp <|> listExp <|> natExp 
 -- >>> testParse basicExp "[a,b,c]"
 -- List [CX (Var {getVar = "a"}),CX (Var {getVar = "b"}),CX (Var {getVar = "c"})]
 
+
+--aux ::[ComplexExp] -> ComplexExp
+--aux [x] = x
+--aux(x:xs) = CApp x $ aux xs
+
 expr :: Parser ComplexExp
-expr = varExp
+expr = do
+    es <- some (basicExp)
+    return $ foldl1 CApp es
 -- >>> testParse expr "\\x -> [x,y,z]"
 -- CLam (Var {getVar = "x"}) (List [CX (Var {getVar = "x"}),CX (Var {getVar = "y"}),CX (Var {getVar = "z"})])
 
